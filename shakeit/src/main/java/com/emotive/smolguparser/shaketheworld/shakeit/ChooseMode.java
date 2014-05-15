a18273f7c544f824e5b168acf4e76fc6bd5d75e1
Alexon-Nik <emotive912@gmail.com>
2014-05-16 00:50:31 +0400

    added some changes

--- shakeit/src/main/java/com/emotive/smolguparser/shaketheworld/shakeit/ChooseMode.java
+++ shakeit/src/main/java/com/emotive/smolguparser/shaketheworld/shakeit/ChooseMode.java
@@ -1,5 +1,6 @@
 package com.emotive.smolguparser.shaketheworld.shakeit;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 
-public class ChooseMode extends ActionBarActivity {
+public class ChooseMode extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +20,8 @@ public class ChooseMode extends ActionBarActivity {
     }
 
     private void initForm() {
-        Button btnSinglePlayer = (Button)findViewById(R.id.btnSinglePlayer);
-        Button btnMultiPlayer = (Button)findViewById(R.id.btnMultiPlayer);
+        Button btnSinglePlayer = (Button) findViewById(R.id.btnSinglePlayer);
+        Button btnMultiPlayer = (Button) findViewById(R.id.btnMultiPlayer);
 
         btnSinglePlayer.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
@@ -37,7 +38,6 @@ public class ChooseMode extends ActionBarActivity {
     }
 
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -52,8 +52,10 @@ public class ChooseMode extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            return true;
+        //do nothing
         }
-        return super.onOptionsItemSelected(item);
-    }
+            return super.onOptionsItemSelected(item);
+        }
+
+
 }
