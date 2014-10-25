447def56225844a5da91d94b469cba3cdb3675b0
Ink1804 <keksman89@gmail.com>
2014-10-25 19:29:36 +0400

    Toast

--- shakeit/src/main/java/com/emotive/smolguparser/shaketheworld/shakeit/SettingsActivity.java
+++ shakeit/src/main/java/com/emotive/smolguparser/shaketheworld/shakeit/SettingsActivity.java
@@ -14,12 +14,13 @@ import android.os.Vibrator;
 import android.os.Bundle;
 import android.preference.DialogPreference;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class SettingsActivity extends Activity {
 
-    public int SHAKE_SENSIVITY_SETTING=25;
-    public boolean check=true;
+    public int SHAKE_SENSIVITY_SETTING = 25;
+    public boolean check = true;
     Vibrator vibro;
     int shakes = 0;
     String strSensivity;
@@ -34,49 +35,54 @@ public class SettingsActivity extends Activity {
         setContentView(R.layout.activity_settings);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensorManager.registerListener(sensorListener,sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    public void Timer(){
-        CountDownTimer cdt = new CountDownTimer(25000,1000) {
+    public void Timer() {
+        CountDownTimer cdt = new CountDownTimer(25000, 1000) {
             @Override
             public void onTick(long l) {
-                if(check){
-                    if(shakes<10){
-                        SHAKE_SENSIVITY_SETTING-=1;
+                if (check) {
+                    if (shakes < 10) {
+                        SHAKE_SENSIVITY_SETTING -= 1;
 
-                    }else{
-                        check=false;
-                        
-                        vibro.vibrate(500);
-                      /* AlertDialog.Builder NEKIT_LOHUDRA = new AlertDialog.Builder(getApplicationContext());
+                    } else {
+                        check = false;
+
+                        //vibro.vibrate(500);
+                        AlertDialog.Builder NEKIT_LOHUDRA = new AlertDialog.Builder(SettingsActivity.this);
                         NEKIT_LOHUDRA.setTitle("Done")
-                        .setMessage("Colibration has ended.")
-                        .setCancelable(false)
-                        .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                                .setMessage("Colibration has ended.")
+                                .setCancelable(false)
+                                .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
 
-                            public void onClick(DialogInterface dialog, int arg1){
-                                dialog.cancel();
-                                Intent intent = new Intent(getApplicationContext(), StartScreen.class);
-                                startActivity(intent);finish();
-                            }
-                        });
+                                    public void onClick(DialogInterface dialog, int arg1) {
+                                        dialog.cancel();
+                                        Intent intent = new Intent(getApplicationContext(), StartScreen.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                });
                         AlertDialog alert = NEKIT_LOHUDRA.create();
                         alert.show();
-                    }*/
-                }}
-            }
-            @Override
-            public void onFinish() {
+                    }
+                }
 
-            }
-        };
-        cdt.start();
+        }
+        @Override
+        public void onFinish () {
+
+        }
     }
+
+    ;
+    cdt.start();
+}
 
 
     public void btn_start_set_Click(View v){
         Timer();
+        Toast.makeText(getApplicationContext(), "asdasdasdasdas", Toast.LENGTH_SHORT).show();
     }
 
     private final SensorEventListener sensorListener = new SensorEventListener() {
